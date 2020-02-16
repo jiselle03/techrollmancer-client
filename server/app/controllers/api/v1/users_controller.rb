934a2853a76dd7913b756057344d00dda4047e54
Jiselle Liu <jiselle03@gmail.com>
2020-02-16 10:51:33 -0800

    Fix typo in users controller

--- server/app/controllers/api/v1/users_controller.rb
+++ server/app/controllers/api/v1/users_controller.rb
@@ -8,10 +8,13 @@ class Api::V1::UsersController < Api::ApplicationController
     def create
         user = User.new user_params
         if user.save
-        session[:user_id] = user.id
-        render json: {id: user.id}
+            session[:user_id] = user.id
+            render json: {id: user.id}
         else
-        render json: { errors: user.errors.full_messages }, status: 422
+            render(
+                json: { errors: user.errors.full_messages }, 
+                status: 422
+            )
         end
     end
 
