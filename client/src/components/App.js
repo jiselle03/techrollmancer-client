6e945f5a6015f7bb256c1281cbddb40679814811
Jiselle Liu <jiselle03@gmail.com>
2020-02-16 12:15:50 -0800

    Convert App from class based component to function based component using hooks

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,8 +1,8 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import './css/NavBar.css';
-// import { CircularProgress } from '@material-ui/core';
+import { CircularProgress } from '@material-ui/core';
 import { User } from '../api/user';
 import { Session } from '../api/session';
 import { NavBar } from './NavBar';
@@ -33,103 +33,101 @@ import { CharacterNewPage } from './character/CharacterNewPage';
 import { CharacterIndexPage } from './character/CharacterIndexPage';
 import { GeneratorPage } from './GeneratorPage';
 
-class App extends Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        currentUser: null,
-        isLoading: true
+const App = () => {
+  const [currentUser, setCurrentUser] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const getUser = useCallback(() => {
+    User.current().then(data => {
+      if (typeof data.id !== "number") {
+        setCurrentUser(null);
+        setIsLoading(false);
+      } else {
+        setCurrentUser(data);
+        setIsLoading(false);
       };
-    };
-  
-    getUser = () => {
-      User.current().then(data => {
-        if (typeof data.id !== "number") {
-          this.setState({ currentUser: null, isLoading: false });
-        } else {
-          this.setState({ currentUser: data, isLoading: false });
-        };
+    });
+  }, []);
+
+  useEffect(() => {
+    User.current().then(currentUser => { 
+        setCurrentUser(currentUser);
+        setIsLoading(false);
       });
-    };
-  
-    destroySession = () => {
-      Session.destroy().then(this.setState({ currentUser: null }));
-    };
-  
-    componentDidMount() {
-      this.getUser();
-    };
-  
-    render() {
-      // if(this.state.isLoading) {
-      //   return(
-      //     <CircularProgress variant="determinate" />
-      //   );
-      // };
-        
-      return (
-        <BrowserRouter>
-            <header>
-              <NavBar 
-                currentUser={this.state.currentUser} 
-                onSignOut={this.destroySession} 
-              />
-            </header>
-            <Switch>
-              <Route exact path="/" component={WelcomePage} />
-              <Route exact path="/libraries" component={Libraries} />
-              <Route exact path="/libraries/races" component={RaceIndexPage} />
-              <Route path="/libraries/races/:slug" component={RaceShowPage} />
-              <Route exact path="/libraries/classes" component={ClassIndexPage} />
-              <Route path="/libraries/classes/:slug" component={ClassShowPage} />
-              <Route exact path="/libraries/spells" component={SpellIndexPage} />
-              <Route path="/libraries/spells/:slug" component={SpellShowPage} />
-              <Route exact path="/libraries/equipment/adventuring-gear" component={AdventuringGearIndexPage} />
-              <Route exact path="/libraries/equipment/mounts-and-vehicles" component={MountVehicleIndexPage} />
-              <Route exact path="/libraries/equipment/tools" component={ToolIndexPage} />
-              <Route exact path="/libraries/equipment/weapons" component={WeaponIndexPage} />
-              <Route exact path="/libraries/equipment/armor" component={ArmorIndexPage} />
-              <Route exact path="/libraries/equipment" component={EquipmentIndexPage} />
-              <Route path="/libraries/equipment/:slug" component={EquipmentShowPage} />
-              <Route exact path="/libraries/conditions" component={ConditionIndexPage} />
-              <AuthRoute 
-                isAuthenticated={!!this.state.currentUser}
-                component={CharacterNewPage}
-                path = "/characters/new"
-                exact
-              />
-              <AuthRoute 
-                isAuthenticated={!!this.state.currentUser}
-                component={CharacterIndexPage}
-                exact path="/characters"
-              />
-              <AuthRoute 
-                isAuthenticated={!!this.state.currentUser}
-                component={CharacterShowPage}
-                path="/characters/:id"
-              />
-              <Route 
-                path="/sign_in"
-                render={routeProps => (
-                  <SignInPage {...routeProps} onSignIn={this.getUser} />
-                )}  
-              />
-              <Route 
-                path="/sign_up"
-                render={routeProps => (
-                  <SignUpPage {...routeProps} onSignUp={this.getUser} />
-                )}  
-              />
-              <Route 
-                path="/generator"
-                component={GeneratorPage}  
-              />
-              <Route component={NotFoundPage} />
-            </Switch>
-        </BrowserRouter>
-      );
-    };
+  }, []);
+
+  const destroySession = () => {
+    Session.destroy().then(setCurrentUser(null));
   };
   
-  export default App;
+  if(isLoading) {
+    return(
+      <CircularProgress variant="determinate" />
+    );
+  };
+      
+  return (
+    <BrowserRouter>
+        <header>
+          <NavBar 
+            currentUser={currentUser} 
+            onSignOut={destroySession} 
+          />
+        </header>
+        <Switch>
+          <Route exact path="/" component={WelcomePage} />
+          <Route exact path="/libraries" component={Libraries} />
+          <Route exact path="/libraries/races" component={RaceIndexPage} />
+          <Route path="/libraries/races/:slug" component={RaceShowPage} />
+          <Route exact path="/libraries/classes" component={ClassIndexPage} />
+          <Route path="/libraries/classes/:slug" component={ClassShowPage} />
+          <Route exact path="/libraries/spells" component={SpellIndexPage} />
+          <Route path="/libraries/spells/:slug" component={SpellShowPage} />
+          <Route exact path="/libraries/equipment/adventuring-gear" component={AdventuringGearIndexPage} />
+          <Route exact path="/libraries/equipment/mounts-and-vehicles" component={MountVehicleIndexPage} />
+          <Route exact path="/libraries/equipment/tools" component={ToolIndexPage} />
+          <Route exact path="/libraries/equipment/weapons" component={WeaponIndexPage} />
+          <Route exact path="/libraries/equipment/armor" component={ArmorIndexPage} />
+          <Route exact path="/libraries/equipment" component={EquipmentIndexPage} />
+          <Route path="/libraries/equipment/:slug" component={EquipmentShowPage} />
+          <Route exact path="/libraries/conditions" component={ConditionIndexPage} />
+          <AuthRoute 
+            isAuthenticated={!!currentUser}
+            component={CharacterNewPage}
+            path = "/characters/new"
+            exact
+          />
+          <AuthRoute 
+            isAuthenticated={!!currentUser}
+            component={CharacterIndexPage}
+            exact path="/characters"
+          />
+          <AuthRoute 
+            isAuthenticated={!!currentUser}
+            component={CharacterShowPage}
+            path="/characters/:id"
+          />
+          <Route 
+            path="/sign_in"
+            render={routeProps => (
+              <SignInPage {...routeProps} onSignIn={getUser} />
+            )}  
+          />
+          <Route 
+            path="/sign_up"
+            render={routeProps => (
+              <SignUpPage {...routeProps} onSignUp={getUser} />
+            )}  
+          />
+          <Route 
+            path="/generator"
+            component={GeneratorPage}  
+          />
+          <Route component={NotFoundPage} />
+        </Switch>
+    </BrowserRouter>
+  );
+};
+  
+export default App;
   
