fe3ac4d77d7ffc345a57f872f0982e6f295fcccb
Jiselle Liu <jiselle03@gmail.com>
2020-02-15 20:06:35 -0800

    Add styling to NotFoundPage

--- client/src/components/NotFoundPage.js
+++ client/src/components/NotFoundPage.js
@@ -1,3 +1,11 @@
 import React from 'react';
 
-export const NotFoundPage = () => <h1>404 Not Found</h1>;
+export const NotFoundPage = () => {
+    return (
+        <div className="error-background">
+            <main className="Main">
+                <h1 className="error">404 Not Found</h1>
+            </main>
+        </div>
+    );
+};
