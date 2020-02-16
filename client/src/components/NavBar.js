2f425ee7667c92634ffcf5a2feaccad48eb8087e
Jiselle Liu <jiselle03@gmail.com>
2020-02-15 23:48:49 -0800

    Swap out card photos for races and classes, edit NavBar class name for css

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 
 import './css/NavBar.css';
-import { makeStyles } from '@material-ui/core/styles';
 import Drawer from '@material-ui/core/Drawer';
 import Button from '@material-ui/core/Button';
 import List from '@material-ui/core/List';
@@ -22,34 +21,7 @@ function ListItemLink(props) {
     return <ListItem button component="a" {...props} />;
 };
 
-const useStyles = makeStyles(theme => ({
-    root: {
-        maxWidth: 360,
-        backgroundColor: "rgba(45,99,127,1)",
-        borderRight: "1px solid rgba(189, 195, 199, 0.5)", 
-        minHeight: "100vh",
-        boxShadow: "5px 0 5px -2px #888",
-        color: "#ffffff",
-        position: "fixed"
-    },
-    nested: {
-        paddingLeft: theme.spacing(9)
-    },
-    nestedRoot: {
-        paddingLeft: theme.spacing(6.75)
-    },
-    nestedDouble: {
-      paddingLeft: theme.spacing(12)
-    },list: {
-      width: 250,
-    },
-    fullList: {
-      width: 'auto',
-    },
-  }));
-
 export const NavBar = ({ currentUser, onSignOut }) => {
-    const classes = useStyles();
     const [state, setState] = useState({
         left: false
       });
@@ -75,7 +47,6 @@ export const NavBar = ({ currentUser, onSignOut }) => {
 
     const sideList = side => (
         <div
-            className={classes.list}
             role="presentation"
             onClick={toggleDrawer(side, false)}
             onKeyDown={toggleDrawer(side, false)}
@@ -93,25 +64,25 @@ export const NavBar = ({ currentUser, onSignOut }) => {
                     LLMANCER
                 </ListItemLink>         
             }
-            className={`${classes.root} mobile-header`}
+            className="NavBar mobile-header"
             >
                 <ListItemLink button href="/sign_in">
-                        <ListItemIcon>
-                            <AccountCircleIcon />
-                        </ListItemIcon>
-                        <ListItemText primary="Sign In" />
+                    <ListItemIcon>
+                        <AccountCircleIcon />
+                    </ListItemIcon>
+                    <ListItemText primary="Sign In" />
                 </ListItemLink>
                 <ListItemLink button href="/sign_up">
-                        <ListItemIcon>
-                            <PersonAddIcon />
-                        </ListItemIcon>
-                        <ListItemText primary="Sign Up" />
+                    <ListItemIcon>
+                        <PersonAddIcon />
+                    </ListItemIcon>
+                    <ListItemText primary="Sign Up" />
                 </ListItemLink>
                 <ListItemLink button href="/characters">
-                        <ListItemIcon>
-                            <CreateIcon />
-                        </ListItemIcon>
-                        <ListItemText primary="Characters" />
+                    <ListItemIcon>
+                        <CreateIcon />
+                    </ListItemIcon>
+                    <ListItemText primary="Characters" />
                 </ListItemLink>
                 <ListItemLink button href="/generator">
                     <ListItemIcon>
@@ -135,38 +106,38 @@ export const NavBar = ({ currentUser, onSignOut }) => {
                 <Divider />
 
                 <List component="div" disablePadding>
-                    <ListItemLink button className={classes.nested} href="/libraries/races">
-                        <ListItemText primary="Races" />
+                    <ListItemLink button href="/libraries/races">
+                        <ListItemText className="nested" primary="Races" />
                     </ListItemLink>
-                    <ListItemLink button className={classes.nested} href="/libraries/classes">
-                        <ListItemText primary="Classes" />
+                    <ListItemLink button href="/libraries/classes">
+                        <ListItemText className="nested" primary="Classes" />
                     </ListItemLink>
-                    <ListItemLink button className={classes.nested} href="/libraries/conditions">
-                        <ListItemText primary="Conditions" />
+                    <ListItemLink button href="/libraries/conditions">
+                        <ListItemText className="nested" primary="Conditions" />
                     </ListItemLink>
-                    <ListItemLink button className={classes.nested} href="/libraries/spells">
-                        <ListItemText primary="Spells" />
+                    <ListItemLink button href="/libraries/spells">
+                        <ListItemText className="nested" primary="Spells" />
                     </ListItemLink>
                     <ListItem button onClick={() => handleClick("equipment")}>
-                    <ListItemText primary="Equipment:" className={classes.nestedRoot} />
+                    <ListItemText className="nested" primary="Equipment:" />
                     </ListItem>
                 </List>
 
                 <List component="div" disablePadding>
-                    <ListItemLink button className={classes.nestedDouble} href="/libraries/equipment/adventuring-gear">
-                        <ListItemText primary="Adventuring Gear" />
+                    <ListItemLink button href="/libraries/equipment/adventuring-gear">
+                        <ListItemText className="double-nested" primary="Adventuring Gear" />
                     </ListItemLink>
-                    <ListItemLink button className={classes.nestedDouble} href="/libraries/equipment/armor">
-                        <ListItemText primary="Armor" />
+                    <ListItemLink button href="/libraries/equipment/armor">
+                        <ListItemText className="double-nested" primary="Armor" />
                     </ListItemLink>
-                    <ListItemLink button className={classes.nestedDouble} href="/libraries/equipment/mounts-and-vehicles">
-                        <ListItemText primary="Mounts and Vehicles" />
+                    <ListItemLink button href="/libraries/equipment/mounts-and-vehicles">
+                        <ListItemText className="double-nested" primary="Mounts and Vehicles" />
                     </ListItemLink>
-                    <ListItemLink button className={classes.nestedDouble} href="/libraries/equipment/tools">
-                        <ListItemText primary="Tools" />
+                    <ListItemLink button href="/libraries/equipment/tools">
+                        <ListItemText className="double-nested" primary="Tools" />
                     </ListItemLink>
-                    <ListItemLink button className={classes.nestedDouble} href="/libraries/equipment/weapons">
-                        <ListItemText primary="Weapons" />
+                    <ListItemLink button href="/libraries/equipment/weapons">
+                        <ListItemText className="double-nested" primary="Weapons" />
                     </ListItemLink>
                 </List>
             </List>
@@ -184,107 +155,106 @@ export const NavBar = ({ currentUser, onSignOut }) => {
                     <i className="fas fa-dice-d20 fa fa-spin-hover"></i>
                     LLMANCER
                 </ListItemLink>
-                <Button onClick={toggleDrawer('left', true)}>MENU</Button>
+                <Button onClick={toggleDrawer('left', true)} className="MENU">MENU</Button>
                 <Drawer open={state.left} onClose={toggleDrawer('left', false)}>
                     {sideList('left')}
                 </Drawer>
             </div>
 
-            <div className="NavBar" id="non-mobile">
-                <List
-                component="nav"
-                aria-labelledby="nested-list-subheader"
-                subheader={
-                    <ListItemLink button 
-                        href="/"
-                        className="logo"
-                    >
-                        TECHR
-                        <i className="fas fa-dice-d20 fa fa-spin-hover"></i>
-                        LLMANCER
-                    </ListItemLink>         
-                }
-                className={classes.root}
+            <List
+            component="nav"
+            aria-labelledby="nested-list-subheader"
+            subheader={
+                <ListItemLink button 
+                    href="/"
+                    className="logo"
                 >
-                    <ListItemLink button href="/sign_in">
-                            <ListItemIcon>
-                                <AccountCircleIcon />
-                            </ListItemIcon>
-                            <ListItemText primary="Sign In" />
+                    TECHR
+                    <i className="fas fa-dice-d20 fa fa-spin-hover"></i>
+                    LLMANCER
+                </ListItemLink>         
+            }
+            className="NavBar"
+            id="non-mobile"
+            >
+                <ListItemLink button href="/sign_in">
+                        <ListItemIcon>
+                            <AccountCircleIcon />
+                        </ListItemIcon>
+                        <ListItemText primary="Sign In" />
+                </ListItemLink>
+                <ListItemLink button href="/sign_up">
+                        <ListItemIcon>
+                            <PersonAddIcon />
+                        </ListItemIcon>
+                        <ListItemText primary="Sign Up" />
+                </ListItemLink>
+                <ListItemLink button href="/characters">
+                        <ListItemIcon>
+                            <CreateIcon />
+                        </ListItemIcon>
+                        <ListItemText primary="Characters" />
                     </ListItemLink>
-                    <ListItemLink button href="/sign_up">
-                            <ListItemIcon>
-                                <PersonAddIcon />
-                            </ListItemIcon>
-                            <ListItemText primary="Sign Up" />
-                    </ListItemLink>
-                    <ListItemLink button href="/characters">
-                            <ListItemIcon>
-                                <CreateIcon />
-                            </ListItemIcon>
-                            <ListItemText primary="Characters" />
+                <ListItemLink button href="/generator">
+                    <ListItemIcon>
+                        <i className="fas fa-dice MuiSvgIcon-root"></i>
+                    </ListItemIcon>
+                    <ListItemText primary="Generator" />
+                </ListItemLink>
+                <ListItemLink button href="/scheduler">
+                    <ListItemIcon>
+                        <EventAvailableIcon />
+                    </ListItemIcon>
+                    <ListItemText primary="Scheduler" />
+                </ListItemLink>
+                <ListItem button onClick={() => handleClick("libraries")}>
+                        <ListItemIcon>
+                            <SearchIcon />
+                        </ListItemIcon>
+                        <ListItemText primary="Libraries" />
+                        {librariesOpen ? <ExpandLess /> : <ExpandMore />}
+                </ListItem>
+                <Divider />
+                <Collapse in={librariesOpen} timeout="auto" unmountOnExit>
+                    <List component="div" disablePadding>
+                        <ListItemLink button href="/libraries/races">
+                            <ListItemText className="nested" primary="Races" />
                         </ListItemLink>
-                    <ListItemLink button href="/generator">
-                        <ListItemIcon>
-                            <i className="fas fa-dice MuiSvgIcon-root"></i>
-                        </ListItemIcon>
-                        <ListItemText primary="Generator" />
-                    </ListItemLink>
-                    <ListItemLink button href="/scheduler">
-                        <ListItemIcon>
-                            <EventAvailableIcon />
-                        </ListItemIcon>
-                        <ListItemText primary="Scheduler" />
-                    </ListItemLink>
-                    <ListItem button onClick={() => handleClick("libraries")}>
-                            <ListItemIcon>
-                                <SearchIcon />
-                            </ListItemIcon>
-                            <ListItemText primary="Libraries" />
-                            {librariesOpen ? <ExpandLess /> : <ExpandMore />}
-                    </ListItem>
-                    <Divider />
-                    <Collapse in={librariesOpen} timeout="auto" unmountOnExit>
-                        <List component="div" disablePadding>
-                            <ListItemLink button className={classes.nested} href="/libraries/races">
-                                <ListItemText primary="Races" />
-                            </ListItemLink>
-                            <ListItemLink button className={classes.nested} href="/libraries/classes">
-                                <ListItemText primary="Classes" />
-                            </ListItemLink>
-                            <ListItemLink button className={classes.nested} href="/libraries/conditions">
-                                <ListItemText primary="Conditions" />
-                            </ListItemLink>
-                            <ListItemLink button className={classes.nested} href="/libraries/spells">
-                                <ListItemText primary="Spells" />
-                            </ListItemLink>
-                            <ListItem button onClick={() => handleClick("equipment")}>
-                            <ListItemText primary="Equipment" className={classes.nestedRoot} />
-                            {equipmentOpen ? <ExpandLess /> : <ExpandMore />}
-                            </ListItem>
-                        </List>
-                    </Collapse>
-                    <Collapse in={equipmentOpen} timeout="auto" unmountOnExit>
-                        <List component="div" disablePadding>
-                            <ListItemLink button className={classes.nestedDouble} href="/libraries/equipment/adventuring-gear">
-                                <ListItemText primary="Adventuring Gear" />
-                            </ListItemLink>
-                            <ListItemLink button className={classes.nestedDouble} href="/libraries/equipment/armor">
-                                <ListItemText primary="Armor" />
-                            </ListItemLink>
-                            <ListItemLink button className={classes.nestedDouble} href="/libraries/equipment/mounts-and-vehicles">
-                                <ListItemText primary="Mounts and Vehicles" />
-                            </ListItemLink>
-                            <ListItemLink button className={classes.nestedDouble} href="/libraries/equipment/tools">
-                                <ListItemText primary="Tools" />
-                            </ListItemLink>
-                            <ListItemLink button className={classes.nestedDouble} href="/libraries/equipment/weapons">
-                                <ListItemText primary="Weapons" />
-                            </ListItemLink>
-                        </List>
-                    </Collapse>
-                </List>
-            </div>
+                        <ListItemLink button href="/libraries/classes">
+                            <ListItemText className="nested" primary="Classes" />
+                        </ListItemLink>
+                        <ListItemLink button href="/libraries/conditions">
+                            <ListItemText className="nested" primary="Conditions" />
+                        </ListItemLink>
+                        <ListItemLink button href="/libraries/spells">
+                            <ListItemText className="nested" primary="Spells" />
+                        </ListItemLink>
+                        <ListItem button onClick={() => handleClick("equipment")}>
+                        <ListItemText className="nested" primary="Equipment" />
+                        {equipmentOpen ? <ExpandLess /> : <ExpandMore />}
+                        </ListItem>
+                    </List>
+                </Collapse>
+                <Collapse in={equipmentOpen} timeout="auto" unmountOnExit>
+                    <List component="div" disablePadding>
+                        <ListItemLink button href="/libraries/equipment/adventuring-gear">
+                            <ListItemText className="double-nested" primary="Adventuring Gear" />
+                        </ListItemLink>
+                        <ListItemLink button href="/libraries/equipment/armor">
+                            <ListItemText className="double-nested" primary="Armor" />
+                        </ListItemLink>
+                        <ListItemLink button href="/libraries/equipment/mounts-and-vehicles">
+                            <ListItemText className="double-nested" primary="Mounts and Vehicles" />
+                        </ListItemLink>
+                        <ListItemLink button href="/libraries/equipment/tools">
+                            <ListItemText className="double-nested" primary="Tools" />
+                        </ListItemLink>
+                        <ListItemLink button href="/libraries/equipment/weapons">
+                            <ListItemText className="double-nested" primary="Weapons" />
+                        </ListItemLink>
+                    </List>
+                </Collapse>
+            </List>
         </div>
     );
-}
+};
--- client/src/components/library/RaceShowPage.js
+++ client/src/components/library/RaceShowPage.js
@@ -27,75 +27,77 @@ export const RaceShowPage = props => {
     };
 
     return (
-        <main className="Main">
-            <h2>{race.name}</h2>
+        <div className={`${race.slug}-background Show-Container`}>
+            <main className="Main">
+                <h2>{race.name}</h2>
 
-            <div className={race.desc ? null : "hidden"}>
-                <h3>{race.name} Traits</h3>
-                <p>{race.desc}</p>
-            </div>
+                <div className={race.desc ? null : "hidden"}>
+                    <h3>{race.name} Traits</h3>
+                    <p>{race.desc}</p>
+                </div>
 
-            <div className={race.asi_desc ? null : "hidden"}>
-                <h3>Ability Score Increase</h3>
-                <p>{race.asi_desc}</p>
-            </div>
-            
-            <div className={race.age ? null : "hidden"}>
-                <h3>Age</h3>
-                <p>{race.age}</p>
-            </div>
+                <div className={race.asi_desc ? null : "hidden"}>
+                    <h3>Ability Score Increase</h3>
+                    <p>{race.asi_desc}</p>
+                </div>
+                
+                <div className={race.age ? null : "hidden"}>
+                    <h3>Age</h3>
+                    <p>{race.age}</p>
+                </div>
 
-            <div className={race.alignment ? null : "hidden"}>
-                <h3>Alignment</h3>
-                <p>{race.alignment}</p>
-            </div>
+                <div className={race.alignment ? null : "hidden"}>
+                    <h3>Alignment</h3>
+                    <p>{race.alignment}</p>
+                </div>
 
-            <div className={race.size ? null : "hidden"}>
-                <h3>Size</h3>
-                <p>{race.size}</p>
-            </div>
+                <div className={race.size ? null : "hidden"}>
+                    <h3>Size</h3>
+                    <p>{race.size}</p>
+                </div>
 
-            <div className={race.speed_desc ? null : "hidden"}>
-                <h3>Speed</h3>
-                <p>{race.speed_desc}</p>
-            </div>
+                <div className={race.speed_desc ? null : "hidden"}>
+                    <h3>Speed</h3>
+                    <p>{race.speed_desc}</p>
+                </div>
 
-            <div className={race.languages ? null : "hidden"}>
-                <h3>Languages</h3>
-                <p>{race.languages}</p>
-            </div>
+                <div className={race.languages ? null : "hidden"}>
+                    <h3>Languages</h3>
+                    <p>{race.languages}</p>
+                </div>
 
-            <div className={race.vision ? "" : "hidden"}>
-                <p dangerouslySetInnerHTML={{
-                    __html: utils.getBlurb(race.vision)
-                }}></p>
-            </div>
+                <div className={race.vision ? null : "hidden"}>
+                    <p dangerouslySetInnerHTML={{
+                        __html: utils.getBlurb(race.vision)
+                    }}></p>
+                </div>
 
-            <div className={race.traits ? "" : "hidden"}>
-                <p dangerouslySetInnerHTML={{
-                    __html: utils.getBlurb(race.traits)
-                }}></p>
-            </div>
-            
-            <div className={race.subraces ? "" : "hidden"}>
-                <h2>Subraces</h2>
+                <div className={race.traits ? null : "hidden"}>
+                    <p dangerouslySetInnerHTML={{
+                        __html: utils.getBlurb(race.traits)
+                    }}></p>
+                </div>
+                
+                <div className={race.subraces ? null : "hidden"}>
+                    <h2>Subraces</h2>
 
-                {race.subraces.map(subrace => (
-                    <div className="subrace">
-                        <h3>{subrace.name}</h3>
+                    {race.subraces.map(subrace => (
+                        <div className="subrace">
+                            <h3>{subrace.name}</h3>
 
-                        <h3>Description</h3>
-                        <p>{subrace.desc}</p>
+                            <h3>Description</h3>
+                            <p>{subrace.desc}</p>
 
-                        <h3>Ability Score Improvement</h3>
-                        <p>{subrace.asi_desc}</p>
+                            <h3>Ability Score Improvement</h3>
+                            <p>{subrace.asi_desc}</p>
 
-                        <p dangerouslySetInnerHTML={{
-                            __html: utils.getBlurb(subrace.traits)
-                        }}></p>
-                    </div>
-                ))}
-            </div>
-        </main>
+                            <p dangerouslySetInnerHTML={{
+                                __html: utils.getBlurb(subrace.traits)
+                            }}></p>
+                        </div>
+                    ))}
+                </div>
+            </main>
+        </div>
     );
 };
