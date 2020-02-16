1774c42d7a2572f55ae2cf2404b3db9b03529fe5
Jiselle Liu <jiselle03@gmail.com>
2020-02-16 10:50:21 -0800

    Seed spells and test user, character, and proficiencies

--- server/db/seeds.rb
+++ server/db/seeds.rb
@@ -6,9 +6,28 @@ require "json"
 spell_file = File.open "./db/data_provider/spells.json"
 spell_data = JSON.load spell_file
 
-Character.destroy_all()
-User.destroy_all()
 Spell.destroy_all()
+User.destroy_all()
+Character.destroy_all()
+Proficiency.destroy_all()
+
+spell_data.map do |spell|
+    Spell.create(
+        slug: spell["slug"],
+        name: spell["name"],
+        desc: spell["desc"],
+        higher_level: spell["higher_level"],
+        range: spell["range"],
+        components: spell["components"],
+        material: spell["material"],
+        ritual: spell["ritual"],
+        duration: spell["duration"],
+        concentration: spell["concentration"],
+        casting_time: spell["casting_time"],
+        level_int: spell["level_int"],
+        school: spell["school"]
+    )
+end
 
 PASSWORD = "supersecret"  
 
@@ -19,7 +38,7 @@ super_user = User.create(
     is_admin: true
 )
 
-Character.create(
+c = Character.create(
     name: "Ja'el Willow",
     race: "Half-Elf",
     level: 10,
@@ -41,16 +60,20 @@ Character.create(
     user_id: 1
 )
 
-Proficiency.create(
-    str_save: true,
-    dex_save: true,
-    athletics: true,
-    deception: true,
-    history: true,
-    medicine: true,
-    religion: true,
-    stealth: true,
-    character_id: 1
-)
+if c.valid?
+    Proficiency.create(
+        str_save: true,
+        dex_save: true,
+        athletics: true,
+        deception: true,
+        history: true,
+        medicine: true,
+        religion: true,
+        stealth: true,
+        character_id: 1
+    )
+end
 
-puts Cowsay.say("Generated #{User.count} users and #{Character.count} characters.", :tux)
+puts Cowsay.say("Generated #{Spell.count} spells.", :dragon)
+puts Cowsay.say("Generated #{User.count} users.", :tux)
+puts Cowsay.say("Generated #{Character.count} characters.", :frogs)
