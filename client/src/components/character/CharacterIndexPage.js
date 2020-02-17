9ba5ca575b7483629225fafe325f7547f4c8c566
Jiselle Liu <jiselle03@gmail.com>
2020-02-16 16:06:47 -0800

    Have CharacterIndexPage render message if user has no characters, display characters as cards

--- client/src/components/character/CharacterIndexPage.js
+++ client/src/components/character/CharacterIndexPage.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 import '../css/Index.css';
 import { Character } from '../../api/character';
-import { CircularProgress } from "@material-ui/core";
+import { CircularProgress } from '@material-ui/core';
 import { Fab } from '@material-ui/core';
 import AddIcon from '@material-ui/icons/Add';
 import Card from '@material-ui/core/Card';
@@ -16,14 +16,8 @@ export const CharacterIndexPage = () => {
 
     useEffect(() => {
         Character.all().then(characters => { 
-            if (characters.data.length > 0) {
-                setCharacters(characters.data);
-                setIsLoading(false);
-            } else {
-                setCharacters([]);
-                setIsLoading(false);
-            }
-            
+            setCharacters(characters);
+            setIsLoading(false);
           });
     }, []);
 
@@ -33,18 +27,39 @@ export const CharacterIndexPage = () => {
         );
     };
 
+    if (!characters) {
+        return(
+            <div className="character-index-background">
+                <main className="Main">
+                    <h1>Characters</h1>
+                    <Link 
+                        to={"/characters/new"} 
+                        className="link" 
+                    >
+                        <h4>You have not created any characters.</h4>
+                        <h4>Create your first character!</h4>
+                    </Link>
+                    <Link 
+                    exact to={`/characters/new`} 
+                    className="add-link"
+                >
+                    <Fab 
+                        color="secondary" 
+                        aria-label="add"
+                        className="add-button"
+                    >
+                        <AddIcon />
+                    </Fab>
+                </Link>
+                </main>
+            </div>
+        );
+    };
+
     return (
         <div className="character-index-background">
             <main className="Main">
                 <h1>Characters</h1>
-                    <div className={characters.length > 0 ? null : "hidden"}>
-                        <Link 
-                            to={"/characters/new"} 
-                            className="link" 
-                        >
-                            <h1>Create your first character!</h1>
-                        </Link>
-                    </div>
 
                     <div id="grid-container">
                     {characters.map(character => (
@@ -53,11 +68,12 @@ export const CharacterIndexPage = () => {
                                 to={`/characters/${character.id}`} 
                                 className="link" 
                             >
-                                <Card className={character.name}>
+                                <Card className={`${character.name} card`}>
                                     <CardMedia
-                                        image={character.photo_url}
                                         title={character.name}
-                                    />
+                                    >
+                                        <img src={character.photo_url} alternate={character.name} />
+                                    </CardMedia>
                                     <CardContent className="content">
                                         <h5 className="race-name">{character.name}</h5>
                                     </CardContent>
