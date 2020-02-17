9bf9d4f556e7e954c2dfbf16a823fd1f14ff6698
Jiselle Liu <jiselle03@gmail.com>
2020-02-16 23:38:36 -0800

    Change association between character and proficiency from has_many to has_one

--- server/app/serializers/character_serializer.rb
+++ server/app/serializers/character_serializer.rb
@@ -1,6 +1,7 @@
 class CharacterSerializer < ActiveModel::Serializer
   attributes :id, :name, :race, :class_1, :class_2, :class_3, :level, :photo_url, :created_at, :updated_at,
-             :hp, :alignment, :str, :dex, :con, :int, :wis, :cha, :armor_class, :speed
+             :hp, :alignment, :str, :dex, :con, :int, :wis, :cha, :armor_class, :speed,
+             :proficiency
 
   belongs_to :user, key: :player
   
