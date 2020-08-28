5b0e8e1b18cfa0168ec7055848907142d244eeae
Jiselle Liu <jiselle03@gmail.com>
2020-08-28 11:37:46 -0700

    Remove no-cors mode

--- client/src/api/user.js
+++ client/src/api/user.js
@@ -3,14 +3,12 @@ import baseUrl from "../config";
 const User = {
     current() {
         return fetch(`${baseUrl}/users/current`, {
-            mode: "no-cors",
             method: "GET",
             credentials: "include"
         }).then(res => res.json());
     },
     create(params) {
         return fetch (`${baseUrl}/users`, {
-            mode: "no-cors",
             method: "POST",
             credentials: "include",
             headers: {
