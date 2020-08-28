b58a6d0693031b5346116a5e2cb2f976d59bb36f
Jiselle Liu <jiselle03@gmail.com>
2020-08-28 10:59:46 -0700

    update CORS origins

--- server/config/application.rb
+++ server/config/application.rb
@@ -40,7 +40,7 @@ module Techrollmancer
 
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins "herokuapp.com", "http://techrollmancer.herokuapp.com"
+        origins "techrollmancer.herokuapp.com",
         resource '/api/*',
         headers: :any,
         credentials: true,
