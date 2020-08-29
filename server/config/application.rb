c294f9fd15277ea3bc36cf7544a60c05e525a7d1
Jiselle Liu <jiselle03@gmail.com>
2020-08-28 17:10:00 -0700

    add localhost:5500 to CORS origins config

--- server/config/application.rb
+++ server/config/application.rb
@@ -40,13 +40,11 @@ module Techrollmancer
 
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins 'techrollmancer.herokuapp.com'
-        resource(
-          '/api/*',
+        origins 'techrollmancer.herokuapp.com', 'localhost:5500'
+        resource '/api/*',
           headers: :any,
           credentials: true,
           methods: [:get, :post, :put, :patch, :delete, :options]
-        )
       end
     end
     
