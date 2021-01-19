da12df759d3bf4831628aeb20c2875677c85bac1
Jiselle Liu <jiselle03@gmail.com>
2021-01-18 22:12:17 -0800

    Switch CardStyle to styled-components

--- client/src/components/styles/CardStyle.js
+++ client/src/components/styles/CardStyle.js
@@ -1,29 +1,20 @@
 import React from 'react';
+import styled from 'styled-components';
 
-export const CardStyle = props => {
-    const { children, image, imageSize, dice } = props;
-    
-    return(
-        <div 
-            style={{
-                width: dice ? "7em" : "12em",
-                height: dice ? "7em" : "20em",
-                margin: "1em",
-                borderRadius: "5px",
-                boxShadow: "5px 0 5px -2px #888",
-                position: "relative",
-                backgroundColor: dice ? "#fff" : "rgba(45,99,127,1)",
-                backgroundRepeat: "no-repeat",
-                backgroundImage: `url(${image})`,
-                backgroundSize: imageSize || "100%",
-                backgroundPositionX: "center",
-                cursor: "pointer"
-            }}
-        >
-            {children}
-        </div>
-    );
-};
+export const CardStyle = styled.div`
+    width: ${({ dice }) => dice ? "7em" : "12em"};
+    height: ${({ dice }) => dice ? "7em" : "20em"};
+    margin: 1em;
+    border-radius: 5px;
+    box-shadow: 5px 0 5px -2px #888;
+    position: "relative";
+    background-color: ${({ dice }) => dice ? "#fff" : "rgba(45,99,127,1)"};
+    background-repeat: no-repeat;
+    background-image: url(${({image}) => image});
+    background-size: ${({imageSize}) => imageSize || "100%"};
+    background-position-x: center;
+    cursor: pointer;
+`;
 
 export const CardContentStyle = {
     content: {
