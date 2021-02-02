623a3f47e3cbb71480bec5897936284b23b847ec
Jiselle Liu <jiselle03@gmail.com>
2021-02-02 14:18:45 -0800

    Switch to Heading and Text components

--- client/src/components/character/CharacterFeatures.js
+++ client/src/components/character/CharacterFeatures.js
@@ -22,90 +22,80 @@ const CharacterFeatures = props => {
                 {id === 1 && (
                 <>
                     <Card className="features">
-                        <h2>
-                            Ki - Class
-                        </h2>
+                        <Heading as="h2">Ki - Class</Heading>
 
-                        <p>
+                        <Text>
                             Starting at 2nd level, your Training allows you to harness the mystic energy of ki. Your access to this energy is represented by a number of ki points. Your monk level determines the number of points you have, as shown in the Ki Points column of the Monk table.
-                        </p>
+                        </Text>
 
-                        <p>
+                        <Text>
                             You can spend these points to fuel various ki features. You start knowing three such features: Flurry of Blow, Patient Defense, and Step of the Wind. You learn more ki features as you gain levels in this class.
-                        </p>
+                        </Text>
 
-                        <p>
+                        <Text>
                             When you spend a ki point, it is unavailable until you finish a short or Long Rest, at the end of which you draw all of your expended ki back into yourself. You must spend at least 30 minutes of the rest meditating to regain your ki points.
-                        </p>
+                        </Text>
                             
-                        <p>
+                        <Text>
                             Some of your ki features require your target to make a saving throw to resist the feature's effects. The saving throw DC is calculated as follows:
-                        </p>
+                        </Text>
                             
-                        <p>
+                        <Text>
                             Ki save DC = 8 + your proficiency bonus + your Wisdom modifier
-                        </p>
+                        </Text>
                             
-                        <p>
+                        <Text>
                             <strong>Flurry of Blows:</strong> Immediately after you take the Attack action on Your Turn, you can spend 1 ki point to make two unarmed strikes as a Bonus Action.
-                        </p>
+                        </Text>
 
-                        <p>
+                        <Text>
                             <strong>Patient Defense:</strong> You can spend 1 ki point to take the Dodge action as a Bonus Action on Your Turn.
-                        </p>
+                        </Text>
                         
-                        <p>
+                        <Text>
                             <strong>Step of the Wind:</strong> You can spend 1 ki point to take the Disengage or Dash action as a Bonus Action on Your Turn, and your jump distance is doubled for the turn.
-                        </p>
+                        </Text>
                     </Card>
 
                     <Card className="features">
-                        <h2>
-                            Unarmored Movement - Class
-                        </h2>
+                        <Heading as="h2">Unarmored Movement - Class</Heading>
 
-                        <p>
+                        <Text>
                             Starting at 2nd level, your speed increases by 10 feet while you are not wearing armor or wielding a Shield. This bonus increases when you reach certain monk levels, as shown in the Monk table.
-                        </p>
-                        <p>
+                        </Text>
+                        <Text>
                             At 9th level, you gain the ability to move along vertical surfaces and across liquids on Your Turn without Falling during the move.
-                        </p>
+                        </Text>
                     </Card>
 
                     <Card className="features">
-                        <h2>
-                            Unarmored Defense - Class
-                        </h2>
+                        <Heading as="h2">Unarmored Defense - Class</Heading>
 
-                        <p>
+                        <Text>
                             Beginning at 1st level, while you are wearing no armor and not wielding a Shield, your AC equals 10 + your Dexterity modifier + your Wisdom modifier.
-                        </p>
+                        </Text>
                     </Card>
 
                     <Card className="features">
-                        <h2>
-                            Darkvision - Race
-                        </h2>
+                        <Heading as="h2">Darkvision - Race</Heading>
 
-                        <p>
+                        <Text>
                             Thanks to your elf blood, you have superior vision in dark and dim Conditions. You can see in dim light within 60 feet of you as if it were bright light, and in Darkness as if it were dim light. You can’t discern color in Darkness, only shades of gray.    
-                        </p>
+                        </Text>
                     </Card>
 
                     <Card className="features">
-                        <h2>
-                            Fey Ancestry - Race
-                        </h2>
+                        <Heading as="h2">Fey Ancestry - Race</Heading>
 
-                        <p>
+                        <Text>
                             You have advantage on saving throws against being Charmed, and magic can’t put you to sleep.
-                        </p>
+                        </Text>
                     </Card>
                 </>
                 )}
                 
                 {id !== 1 && (
-                    <h5>You have not added any features for this character.</h5>
+                    <Heading as="h5">You have not added any features for this character.</Heading>
                 )}
                 
             </div>
